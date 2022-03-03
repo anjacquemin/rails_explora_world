def3fbfd412a702149cf3f54bf99e4cb98755ce6
Anthony Jacquemin <an.jacquemin@gmail.com>
2022-03-03 16:05:53 +0100

    video

--- db/schema.rb
+++ db/schema.rb
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 2022_03_03_103508) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "url_room"
     t.index ["offer_id"], name: "index_slots_on_offer_id"
     t.index ["user_id"], name: "index_slots_on_user_id"
   end
