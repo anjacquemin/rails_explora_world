b1043568b4c9b3e48bea9a27da93b1dddcc8cdc7
Anthony Jacquemin <an.jacquemin@gmail.com>
2022-03-07 18:15:34 +0100

    enhance design

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,46 @@ ActiveStorage.start()
 
 import "controllers"
 import "bootstrap"
+
+
+
+/*
+// Video handling
+
+import  twilioInit  from '../plugins/twilio.js';
+
+
+
+// pour forcer le rechargement du js quand la page est chargée par turbolinks
+document.addEventListener('turbolinks:load', () => {
+  // Call your functions here, e.g:
+  // initSelect2();
+  if (document.querySelector('.proposal_primary_category')) {
+    initFormProposal();
+  }
+  if (document.querySelector('.twilio-video')) {
+    twilioInit();
+  }
+});
+*/
+
+// const rangeInputs = document.querySelectorAll('input[type="range"]')
+// const numberInput = document.querySelector('input[type="number"]')
+
+// function handleInputChange(e) {
+//   let target = e.target
+//   if (e.target.type !== 'range') {
+//     target = document.getElementById('range')
+//   }
+//   const min = target.min
+//   const max = target.max
+//   const val = target.value
+
+//   target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+// }
+
+// rangeInputs.forEach(input => {
+//   input.addEventListener('input', handleInputChange)
+// })
+
+// numberInput.addEventListener('input', handleInputChange)
